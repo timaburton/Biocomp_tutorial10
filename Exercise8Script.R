ef4e1870ceaa4facf0dc2589e452972db31b636a
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-05 11:27:41 -0400

    Adds code to create a random number

---
+++ Exercise8Script.R
@@ -0,0 +1,8 @@
+###Marlee Shaffer 
+##Exercise 8 
+
+###Part 1
+##Generate a plot similar using the data summarized in UWvMSU to show cumulative score throughout the game for University of Wisconsin vs Michigan State University
+
+df<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep=" " )
+
