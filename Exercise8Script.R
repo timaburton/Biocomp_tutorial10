995084b8fbed7b52b3684f71f47e5262206dc5fe
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-05 11:32:36 -0400

    Adds the beginning of a for loop

--- Exercise8Script.R
+++ Exercise8Script.R
@@ -4,5 +4,10 @@
 ###Part 1
 ##Generate a plot similar using the data summarized in UWvMSU to show cumulative score throughout the game for University of Wisconsin vs Michigan State University
 
-df<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep=" " )
+df<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep=" ")
+
+
+####Part 2 
+###Guess my number 
+rand<- sample(1:100, 1)
 
