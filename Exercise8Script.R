88daf5cf639448006aed001eacebeda9da95bf43
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-05 11:48:57 -0400

    Adds for loop and if else statements for guess my number game

--- Exercise8Script.R
+++ Exercise8Script.R
@@ -9,5 +9,30 @@ df<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep=" ")
 
 ####Part 2 
 ###Guess my number 
+
 rand<- sample(1:100, 1)
+
+for (i in 1:10){
+  print("I'm thinking of a number 1 to 100.")
+  usernum <- readline(prompt = "What is your guess?")
+  if (i < 10){
+    if (usernum > rand) {
+      print("Lower")
+    } else if (usernum < rand){
+        print("Higher")
+    } else {
+        print("Correct")
+      break 
+      }
+  }
+  else {
+    if (usernum == rand){
+      print("Correct")
+    }
+    else {
+      print("Sorry. Thanks for playing.")
+    }
+  }
+}
+
 
