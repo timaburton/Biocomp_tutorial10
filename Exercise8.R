bbdc81175ce9d52cd7fbc891bc7b0f13351a8ecd
Johanna1403 <johannaolesk99@gmail.com>
2021-11-08 23:18:47 -0500

    initial task2

--- Exercise8.R
+++ Exercise8.R
@@ -7,23 +7,45 @@ tail(data)
 sum_UW = 0
 sum_MSU = 0
 
-UW = numeric()
-MSU = numeric()
+UW = 0
+MSU = 0
 
 for (i in 1:length(data$team)) {
   if (data$team[i] == "UW") {
     sum_UW = sum_UW + data$score[i]
-    print("UW")
-    print(sum_UW)
-    UW[i] = paste(sum_UW)
+    UW = c(UW, sum_UW)
   }
   else {
     sum_MSU = sum_MSU + data$score[i]
-    print("MSU")
-    print(sum_MSU)
-    MSU[i] = paste(sum_MSU)
+    MSU = c(MSU, sum_MSU)
   }
 }
-length(MSU)
 
+x_time = 1:length(MSU)
 plot(x_time, MSU, type = 'l')
+xt = 1:length(UW)
+lines(xt, UW)
+
+# Task2
+# Guess my number
+
+rand = round(runif(1, 1, 100))
+
+for (i in 1:10) {
+  input = strtoi(readline(prompt = "Guess the number: "), base = 0L)
+  if (rand < input) {
+    print("Try lower.")
+  }
+  else if (rand > input) {
+    print("Try higher.")
+  }
+  else {
+    print("Well done!")
+    break
+  }
+}
+
+
+
+
+
