23fe87f0adf6e75d983016629bd88b9c80890155
Emily Chen <echen5@nd.edu>
2021-11-10 23:58:58 -0500

    This is my submission for Exercise 08

--- Exercise 08 Submission.R
+++ Exercise 08 Submission.R
@@ -1,4 +1,4 @@
-# Problem 1: Generate a graph using score-by-score info from game in txt file
+## Problem 1: Generate a graph using score-by-score info from game in txt file
 
 # Load the score-by-score information
 UWvMSU<-read.table(file="UWvMSU_1-22-13.txt",header=TRUE,stringsAsFactors=FALSE)
@@ -17,4 +17,28 @@ for(i in 1:length(MSU$score)){
 }
 
 # Plot aggregate scores with time as the other variable
-time<-UWvMSU[UWvMSU$time=='time']
+x1<-UW$time
+x2<-MSU$time
+y1<-totalscoreUW
+y2<-totalscoreMSU
+plot(x1,y1,type='l',xlab='time',ylab='score')
+lines(x2,y2)
+
+
+## Problem 2:Generate "Guess my number" game
+
+# Generate random number from 1-100
+randomnum<-sample(1:100,1)
+# Limit to 10 guesses
+for (i in 1:10){
+  guess<-readline(prompt="Pick a number from 1 to 100")}
+# Generate output "lower", "higher", and "correct"
+if(randomnum<guess){
+  print("lower")
+  }else if(randomnum>guess){
+      print("higher")
+        }else{
+  if(randomnum==guess){
+    print("correct!")
+  }
+}
