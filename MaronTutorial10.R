f9677132fef11c3f423e1d00aaa1e8797b9deff1
Erin Maron <erinmaron@Erins-MacBook-Pro.local>
2021-11-11 14:39:30 -0500

    final copy

--- MaronTutorial10.R
+++ MaronTutorial10.R
@@ -14,8 +14,30 @@ df<-data.frame("Team"=score$team,"Time"=score$time, "Score"=All)
 dfMSU<-head(df,27)
 dfUW<-tail(df,23)
 #Plot 
+par(mar=c(1,1,1,1))
 plot(dfMSU$Time,dfMSU$Score, type="l")
 lines(dfUW$Time, dfUW$Score)
 
 #"Guess My Number" Game 
 #generate a random number between 1:100
+read<-function(){
+  n<-readline(prompt="Guess:")
+  if(!grepl("^[0-9]",n)){
+    return(read())
+  }
+  return(as.integer(n))
+}
+num<-round(runif(1)*100, digits=0)
+guess<-1
+PlayTheGame<-function(guess){
+  while(guess !=num){
+    guess<-read()
+    if(guess<num){
+      print("Higher")
+    } else if (guess>num){
+      print("Lower")
+    }else{
+      print("Correct!")
+    }
+    }
+}
