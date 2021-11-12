b06162b67ed99580b1a4ca348b23a2a5a3694d4e
Riley Ellingsen <rileyellingsen@gmail.com>
2021-11-11 20:00:10 -0500

    commit script for second problem - Guess My Number Game

---
+++ GuessMyNumberGame.Rscript.R
@@ -0,0 +1,29 @@
+##Guess My Number
+
+###A game that allows user to guess a randomly generated number and gives feedback
+###for correct and incorrect guesses.
+
+#Generate a random number between 1 and 100
+range <- 1:100
+sample(range, 1)
+
+randonumber <- sample(range,1)
+
+# Write a for loop that allows a user to guess up to ten times
+# Output 'Lower' if guess is greater than random number
+# Output 'Higher' if guess is less than random number
+# Output 'Correct!' if user guesses random number
+
+for (i in 1:10) {
+  guess <- readline(prompt = "Guess:")
+    if (guess > randonumber){
+      print("Lower")
+    } else if (guess < randonumber){
+      print("Higher")
+    } else {
+      print("Correct!")
+      break
+    }
+  }
+  
+  
